f8736753c5166310314fa6d846d7578134e0af77
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-20 14:22:05 -0300

    Update import of store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Router';
-import { store } from './state/store';
+import store from './state/store';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
