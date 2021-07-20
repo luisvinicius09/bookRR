e6b1c253d97877f2420f2ebd076588fcbbf3ca59
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-20 14:08:51 -0300

    Update index.js to add react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Routes from './Router';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Routes />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
