59d494a755a596ff5d88a24ee61897aa926eca27
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-19 21:17:43 -0300

    Create all necessary files and dependencies

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>DevCamp React Starter</h1>
+      <h2>React Redux router</h2>
+    </div>
+  )
+}
+
+export default App;
