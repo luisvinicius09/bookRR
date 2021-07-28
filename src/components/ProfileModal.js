c420b3a60876ab810e094ce2ed02053014d020f8
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-28 14:58:38 -0300

    Add profile modal file

---
+++ src/components/ProfileModal.js
@@ -0,0 +1,35 @@
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { handleProfileModal } from '../state/actions';
+
+const ProfileModal = ({ isOpen, toggleHandler }) => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  const handlePM = () => {
+    dispatch(handleProfileModal());
+  }
+
+  return (
+    <>
+      {/* <Modal isOpen={isOpen} toggle={toggleHandler}>
+        <ModalHeader>
+          <button onClick={toggleHandler}>Close</button>
+        </ModalHeader>
+        <ModalBody>
+          Works
+        </ModalBody>
+      </Modal> */}
+      <Modal isOpen={state.profile} toggle={handlePM}>
+        <ModalHeader>
+          <button onClick={handlePM}>Close</button>
+        </ModalHeader>
+        <ModalBody>
+          Works
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfileModal;
