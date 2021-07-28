3f1bc07d5292b0616a207185d04402a9a7494715
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-28 14:59:28 -0300

    Add action to handle login and registration

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -4,15 +4,43 @@ const FETCH_REQUEST = 'FETCH_REQUEST';
 
 const FETCH_ERROR = 'FETCH_ERROR';
 
+const carsURL = 'http://localhost:3000/cars';
+
+const logInURL = '';
+
 export const fetchCarsData = (service = axios) => async (dispatch) => {
   dispatch({ type: FETCH_REQUEST });
 
   try {
-    const res = await service.get();
+    const res = await service.get(carsURL);
 
     dispatch({ type: "FETCH_CARS", payload: res.data });
   } catch (error) {
     dispatch({ type: FETCH_ERROR, error });
+  }
+}
+
+export const handleRegistration = (service = axios, data) => async (dispatch) => {
+  dispatch({ type: 'REQUEST' });
+
+  try {
+    const res = await service.post(logInURL, data);
+
+    dispatch({ type: "REGISTER_USER", payload: res.data})
+  } catch (error) {
+    dispatch({ type: "ERROR", error});
+  }
+}
+
+export const handleLogin = (service = axios, data) => async (dispatch) => {
+  dispatch({ type: 'REQUEST' });
+
+  try {
+    const res = await service.post(logInURL, data);
+
+    dispatch({ type: "LOG_IN", payload: res.data})
+  } catch (error) {
+    dispatch({ type: "ERROR", error});
   }
 }
 
@@ -22,4 +50,8 @@ export const handleSideBar = () => ({
 
 export const handleProfileModal = () => ({
   type: "HANDLE_PROFILE",
-})
+});
+
+export const handleCarsModal = () => ({
+  type: "HANDLE_CARS_MODAL",
+});
