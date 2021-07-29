fb45545353e2721810726fc9d3d25c3eeafa5352
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-29 20:48:49 -0300

    Add logic to map cars array, pass to car card and display on screen

--- src/components/CarsModal.js
+++ src/components/CarsModal.js
@@ -1,17 +1,29 @@
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import CarCard from './CarCard';
 
 const CarsModal = ({ toggleHandler }) => {
-  const state = useSelector((state) => state.utils);
+  const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
+  const renderCars = () => {
+    // TODO: Logic if state is loading
+
+    if (!state.carsData.cars) {
+      return <h2>No items to display or something went wrong</h2>
+    }
+    return state.carsData.cars.map((car) => (
+      <CarCard data={car} />
+    ));
+  };
+
   return (
-    <Modal toggle={toggleHandler} isOpen={state.carsModal}>
+    <Modal toggle={toggleHandler} isOpen={state.utils.carsModal}>
       <ModalHeader>
-        Header
+        Header {/* TODO: Search/Filter bar */}
       </ModalHeader>
       <ModalBody>
-        Body
+        {renderCars()}
       </ModalBody>
     </Modal>
   );
