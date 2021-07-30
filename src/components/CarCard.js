18055205180df2e177b1a6b6ec3fd424319efa96
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-29 21:17:24 -0300

    Add css file to style car card

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import styles from '../assets/css/carcard.module.css';
 
 const CarCard = ({ data }) => {
   return (
-    <Link>
+    <Link to={`/cars/${data.id}`} className={styles.cardContainer}>
+      <div className={styles.imgContainer}>
+        <img src={data.image_url} alt={data.name} />
+      </div>
       <div>
         {data.name}
       </div>
