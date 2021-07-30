27b990ed6869c0487e1d5cb22b7dfc45c709994a
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-30 17:07:58 -0300

    Add react-hook-form and yup dependency

--- src/components/ProfileModal.js
+++ src/components/ProfileModal.js
@@ -1,23 +1,45 @@
+import { useState } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { handleProfileModal } from '../state/actions';
 
 const ProfileModal = ({ isOpen, toggleHandler }) => {
+  const [display, setDisplay] = useState(false);
+
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
   const handlePM = () => {
     dispatch(handleProfileModal());
-  }
+  };
+
+
+  const displaySignUp = () => {
+    setDisplay(true);
+  };
+
+  const displaySignIn = () => {
+    setDisplay(false);
+  };
 
   return (
     <>
       <Modal isOpen={isOpen} toggle={toggleHandler}>
         <ModalHeader>
-          <button onClick={toggleHandler}>Close</button>
+          <button type="button" onClick={() => setDisplay(false)}>Sign In</button>
+          <button type="button" onClick={() => setDisplay(true)}>Sign Up</button>
+          <button type="button" onClick={toggleHandler}>Close</button>
         </ModalHeader>
         <ModalBody>
-          Works
+          {!display ? (
+            <div>
+              <h1>One</h1>
+            </div>
+          ) : (
+            <div>
+              <h1>Other</h1>
+            </div>
+          )}
         </ModalBody>
       </Modal>
     </>
