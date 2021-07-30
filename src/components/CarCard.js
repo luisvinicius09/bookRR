1a7edf121fd3fea435a994699741291fb650beae
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-30 15:23:03 -0300

    Add logic to close modals when clicking on car card

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,13 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from '../assets/css/carcard.module.css';
-import { fetchCarData } from '../state/actions';
+import { fetchCarData, handleCloseModals } from '../state/actions';
 
 const CarCard = ({ data }) => {
   const dispatch = useDispatch();
 
   return (
-    <Link to={`/cars/${data.id}`} className={styles.cardContainer} onClick={() => dispatch(fetchCarData(data.id))}>
+    <Link
+      to={`/cars/${data.id}`} 
+      className={styles.cardContainer}
+      onClick={() => {
+        dispatch(fetchCarData(data.id));
+        dispatch(handleCloseModals());
+      }}
+    >
       <div className={styles.imgContainer}>
         <img src={data.image_url} alt={data.name} />
       </div>
