2665f9f940c5d4b0af63bb49f851ecfd92cad195
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-30 11:45:45 -0300

    Add logic to retrieve data from car if store is empty

--- src/views/Car.js
+++ src/views/Car.js
@@ -1,6 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
+import { Container } from 'reactstrap';
+import { fetchCarData } from '../state/actions';
+
 const Car = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const { car_id } = useParams();
+
+  useEffect(() => {
+    if (state.carsData.car === undefined) {
+      dispatch(fetchCarData(axios, car_id));
+    }
+  }, [dispatch, car_id, state.carsData.car]);
+
+  console.log(state.carsData.car);
+
   return (
-    <h1>working</h1>
+    <>
+    {!state.carsData.loading && state.carsData.car && (
+      <Container fluid={true}>
+        <h1>working</h1>
+      </Container>
+    )}
+    </>
   );
 };
 
