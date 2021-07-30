23cb29b6623211edeb35e776f4c36597e8890770
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-30 15:24:10 -0300

    Add logic to make sidebar buttons works

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,21 +1,50 @@
 import styles from '../assets/css/sidebar.module.css';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { handleCloseModals, handleCarsModal, handleProfileModal } from '../state/actions';
 
 const SideBar = () => {
+  const dispatch = useDispatch();
+
+  const closeModals = () => {
+    dispatch(handleCloseModals());
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.titleName}>
         <h1>RentalRR</h1>
       </div>
       <div className={styles.linksWrapper}>
-        <Link to="/home">HOME</Link>
-        <Link to="/cars">CARS</Link>
-        <a href=".">PROFILE</a>
+        <Link
+          to="/home"
+          onClick={closeModals}
+        >
+          HOME
+        </Link>
+        <span
+          onClick={() => {
+            closeModals();
+            dispatch(handleCarsModal());
+          }}
+        >
+          CARS
+        </span>
+        <span
+          onClick={() => {
+            dispatch(handleProfileModal());
+          }}
+        >
+          PROFILE
+        </span>
       </div>
       <div className={styles.footerSidebar}>
-        <a>Instagram</a>
-        <a>Github</a>
-        <a>Linkedin</a>
+        <a href="https://github.com/luisvinicius09">
+          Github
+        </a>
+        <a href="https://www.linkedin.com/in/luis-vinicius/">
+          Linkedin
+        </a>
       </div>
     </div>
   );
