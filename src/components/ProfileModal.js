67f1232edefc56e89e88200968c96b8f3aadc919
Luis Vinicius <luisvinicius0906@gmail.com>
2021-08-01 18:58:06 -0300

    Add logic to display sign up/in if not logged in

--- src/components/ProfileModal.js
+++ src/components/ProfileModal.js
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Modal, ModalHeader, ModalBody, Button, Form, FormGroup, Input, FormFeedback, Label } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { handleProfileModal, handleLogin, handleRegistration } from '../state/actions';
+import { handleLogin, handleRegistration } from '../state/actions';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 const schemaSignIn = yup.object().shape({
-  email: yup.string().email().required("Email is required!"),
+  email: yup.string().email("Please enter a valid email. Ex: example@email.com").required("Email is required!"),
   password: yup.string().required("Password is required!").min(4, "Your password must be 6 characters long!"),
 });
 
 const schemaSignUp = yup.object().shape({
-  email: yup.string().email().required("Email is required!"),
+  username: yup.string().required("Name is required!"),
+  email: yup.string().email("Please enter a valid email. Ex: example@email.com").required("Email is required!"),
   password: yup.string().required("Password is required!").min(4, "Your password must be 6 characters long!"),
+  password_confirmation: yup.string().required("Password confirmation is required!").min(4, "Your password confirmation must be 6 characters long!"),
 });
 
 const ProfileModal = ({ isOpen, toggleHandler }) => {
@@ -47,14 +49,13 @@ const ProfileModal = ({ isOpen, toggleHandler }) => {
   }, [state, dispatch]);
 
   const submitSignIn = (data) => {
+    dispatch(handleLogin(data));
+    console.log(data);
   }
+
   const submitSignUp = (data) => {
     dispatch(handleRegistration(data));
   }
-
-  const handlePM = () => {
-    dispatch(handleProfileModal());
-  };
 
   const displaySignUp = () => {
     setDisplay(true);
@@ -68,86 +69,109 @@ const ProfileModal = ({ isOpen, toggleHandler }) => {
     console.log(errorsSignIn);
   }
 
+  if (errorsSignUp) {
+    console.log(errorsSignUp);
+  }
+
   return (
     <>
-      <Modal isOpen={isOpen} toggle={toggleHandler}>
-        <ModalHeader>
-          <button type="button" onClick={displaySignIn}>Sign In</button>
-          <button type="button" onClick={displaySignUp}>Sign Up</button>
-          <button type="button" onClick={toggleHandler}>Close</button>
-        </ModalHeader>
-        <ModalBody>
-          {!display ? (
-            <div>
-              <h2>Sign In</h2>
-              <Form onSubmit={handleSubmitSignIn(submitSignIn)}>
-                <FormGroup>
-                  <Label for="email">
-                    Email
-                  </Label>
-                  <Input {...registerSignIn('email')} />
-                  <FormFeedback>
-                    {/* {errors.email.message ? errors?.email.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="password">
-                    Password
-                  </Label>
-                  <Input {...registerSignIn('password')} />
-                  <FormFeedback>
-                    {/* {errors.password.message ? errors?.password.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <Button type="submit">Submit</Button>
-              </Form>
-            </div>
-          ) : (
-            <div>
-              <h2>Sign Up</h2>
-              <Form onSubmit={handleSubmitSignUp(submitSignUp)}>
-                <FormGroup>
-                  <Label for="username">
-                    Username
-                  </Label>
-                  <Input {...registerSignUp('username')} />
-                  <FormFeedback>
-                    {/* {errors.username.message ? errors?.username.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="email">
-                    Email
-                  </Label>
-                  <Input {...registerSignUp('email')} />
-                  <FormFeedback>
-                    {/* {errors.email.message ? errors?.email.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="password">
-                    Password
-                  </Label>
-                  <Input {...registerSignUp('password')} />
-                  <FormFeedback>
-                    {/* {errors.password.message ? errors?.password.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="password_confirmation">
-                    Password Confirmation
-                  </Label>
-                  <Input {...registerSignUp('password_confirmation')} />
-                  <FormFeedback>
-                    {/* {errors.password_confirmation.message ? errors?.password_confirmation.message : null} */}
-                  </FormFeedback>
-                </FormGroup>
-                <Button type="submit">Submit</Button>
-              </Form>
-            </div>
-          )}
-        </ModalBody>
-      </Modal>
+      {!state.user.loggedIn ? (
+        <Modal isOpen={isOpen} toggle={toggleHandler}>
+          <ModalHeader>
+            <button type="button" onClick={displaySignIn}>Sign In</button>
+            <button type="button" onClick={displaySignUp}>Sign Up</button>
+            <button type="button" onClick={toggleHandler}>Close</button>
+          </ModalHeader>
+          <ModalBody>
+            {!display ? (
+              <div>
+                <h2>Sign In</h2>
+                <Form onSubmit={handleSubmitSignIn(submitSignIn)}>
+                  <FormGroup>
+                    <Label for="email">
+                      Email
+                    </Label>
+                    <Input type="email" {...registerSignIn('email')} key="signUpEmail" />
+                    <FormFeedback>
+                      {/* {errors.email.message ? errors?.email.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="password">
+                      Password
+                    </Label>
+                    <Input type="password" {...registerSignIn('password')} key="signUpPassowrd" />
+                    <FormFeedback>
+                      {/* {errors.password.message ? errors?.password.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <Button type="submit">Submit</Button>
+                </Form>
+              </div>
+            ) : (
+              <div>
+                <h2>Sign Up</h2>
+                <Form onSubmit={handleSubmitSignUp(submitSignUp)}>
+                  <FormGroup>
+                    <Label for="username">
+                      Username
+                    </Label>
+                    <Input type="text" {...registerSignUp('username')} />
+                    <FormFeedback>
+                      {/* {errorsSignUp.username.message ? errorsSignUp?.username.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="email">
+                      Email
+                    </Label>
+                    <Input type="email" {...registerSignUp('email')} />
+                    <FormFeedback>
+                      {/* {errorsSignUp.email.message ? errorsSignUp?.email.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="password">
+                      Password
+                    </Label>
+                    <Input type="password" {...registerSignUp('password')} />
+                    <FormFeedback>
+                      {/* {errorsSignUp.password.message ? errorsSignUp?.password.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="password_confirmation">
+                      Password Confirmation
+                    </Label>
+                    <Input type="password" {...registerSignUp('password_confirmation')} />
+                    <FormFeedback>
+                      {/* {errorsSignUp.password_confirmation.message ? errorsSignUp?.password_confirmation.message : null} */}
+                    </FormFeedback>
+                  </FormGroup>
+                  <Button type="submit">Submit</Button>
+                </Form>
+              </div>
+            )}
+          </ModalBody>
+        </Modal>
+      ) : <Modal isOpen={isOpen} toggle={toggleHandler}>
+      <ModalHeader>
+        <button type="button" onClick={displaySignIn}>Sign In</button>
+        <button type="button" onClick={displaySignUp}>Sign Up</button>
+        <button type="button" onClick={toggleHandler}>Close</button>
+      </ModalHeader>
+      <ModalBody>
+        {!display ? (
+          <div>
+
+          </div>
+        ) : (
+          <div>
+            lol
+          </div>
+        )}
+      </ModalBody>
+    </Modal>}
     </>
   );
 };
